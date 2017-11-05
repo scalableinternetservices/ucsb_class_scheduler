da47ad0af217b9bf8249aa8c67e4ca05449fd678
evenskari <evenskari@gmail.com>
2017-11-04 19:17:09 -0700

    Load courses from data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+File.open("CourseData.txt").each do |line|
+  parsed_course = JSON.parse(line) #Every line in a the .txt file represents an JSON course object with belonging time objects
+  department = parsed_course["department"]
+  grading_option = parsed_course["grading_option"]
+  name = parsed_course["name"]
+  id = parsed_course["id"]
+  students_enrolled_max  = parsed_course["max_students"]
+  units= parsed_course["units"]
+  description= parsed_course["desc"]
+  instructor= parsed_course["instructors"] #returns instructors separated by comma
+
+  is_graduate_course = parsed_course['is_grad']
+  #Course.create( department,id,description,units,grading_option, students_enrolled_max,instructor)
+  #students_enrolled_actually= parsed_course["enrolled_students"]
+  #prequisites= parsed_course["prequisites"]
+  #is_graduate_course not scraping this as of now
+
+  print(department,grading_option,name,id,description,instructor)
+  course = Course.create(department: department, course_no: id, description: description,
+                         units: units, grading_opts: grading_option,max_class_size: students_enrolled_max,
+                         instructor_id: instructor)
+
+  parsed_course["timeslots"].each do |time|
+    days_to_repeat = time["days"]
+    room = time["location"]
+    start_time = time["start_time"]
+    end_time = time["end_time"]
+    type = time["type"]
+    #enrolled_max = time["enrolled_max"]
+    #enrolled_count = time["enrolled_count"]
+    #instructor = time["instructor"]
+    #Course.times.create(...)
+    print(days_to_repeat,room,start_time,end_time)
+    course.periods.create(start_time: start_time, end_time: end_time, days: days, type: type, room: room)
+    break
+  end
+  break
+end
