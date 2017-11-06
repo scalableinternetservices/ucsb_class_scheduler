ca046e0a29f43862e2cb2ef7a4548e414ff59b58
Even <evenskari@gmail.com>
2017-11-05 23:31:32 -0800

    Seed data to db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 require 'json'
 File.open("data.txt").each do |line|
-
   parsed_course = JSON.parse(line) #Every line in a the .txt file represents an JSON course object with belonging time objects
   department = parsed_course["department"]
   grading_option = parsed_course["grading_option"]
@@ -17,41 +9,45 @@ File.open("data.txt").each do |line|
   units= parsed_course["units"]
   description= parsed_course["desc"]
   instructor_name= parsed_course["instructors"] #returns instructors separated by comma
-
   is_graduate_course = parsed_course['is_grad']
-  #Course.create( department,id,description,units,grading_option, students_enrolled_max,instructor)
-  #students_enrolled_actually= parsed_course["enrolled_students"]
-  #prequisites= parsed_course["prequisites"]
-  #is_graduate_course not scraping this as of now
-
-  print(department+"\n",grading_option+"\n",name+"\n",id+"\n",description+"\n",instructor_name)
+  students_enrolled_actually= parsed_course["enrolled_students"]
+  prerequisites= parsed_course["prequisites"]
   if instructor_name.include? ","
     instructor_name = instructor_name.split(",")[0]
   end
-
-  if Instructor.exists?(id: instructor_name)
-    instructor = Instructor.find(id: instructor_name)
+  if Instructor.exists? instructor_name
+    instructor = Instructor.find(instructor_name)
   else
-    instructor = Instructor.create(instructor_name)
+    instructor = Instructor.create(id: instructor_name, rmp_url: "lol")
   end
-
-  course = instructor.courses.create(dept: department, course_no: id, description: description,
-                         units: units, grading_opts: grading_option,max_class_size: students_enrolled_max,
-                         instructor_id: instructor_name)
-
+  puts id
+  if instructor.courses.exists? course_no: id, dept: department, instructor_id: instructor_name
+    course = instructor.courses.find course_no: id, dept: department, instructor_id: instructor_name
+  else
+    course = instructor.courses.create(dept: department, course_no: id, description: description,
+                                       units: units, grading_opts: grading_option,max_class_size: students_enrolled_max,
+                                       instructor_id: instructor_name, is_graduate_course: is_graduate_course,
+                                       prerequisites: prerequisites, currently_enrolled_students: students_enrolled_actually,
+                                       full_name: name)
+  end
   parsed_course["timeslots"].each do |time|
-    days_to_repeat = time["days"]
+    days = time["days"]
     room = time["location"]
     start_time = time["start_time"]
     end_time = time["end_time"]
     type = time["type"]
-    #enrolled_max = time["enrolled_max"]
-    #enrolled_count = time["enrolled_count"]
-    #instructor = time["instructor"]
-    #Course.times.create(...)
-    print(days_to_repeat,room,start_time,end_time) #gteiorio  
-    instructor.courses.periods.create(start_time: start_time, end_time: end_time, days: days, type: type, room: room)
-    break
+    enrolled_max = time["enrolled_max"]
+    enrolled_count = time["enrolled_count"]
+    instructor = time["instructor"]
+    puts 'startTime:'+start_time
+    if start_time == ""
+      start_time = "00:00pm"
+    end
+
+    if end_time == ""
+      end_time = "00:00pm"
+    end
+    course.periods.create(start_time: start_time, end_time: end_time, days: days, period_type: type, location: room,
+                          currently_enrolled_students: enrolled_count, max_class_size: enrolled_max, instructor_name: instructor)
   end
-  break
 end
