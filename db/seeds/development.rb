01183852adeb7df56a21d9673431d9e391968f54
dannycho7 <dannycho7@gmail.com>
2017-11-29 02:57:52 -0800

    Add Course seeding -- 1 million courses per dept

--- db/seeds/development.rb
+++ db/seeds/development.rb
@@ -18,4 +18,37 @@ end
 
 departments = JSON.parse(File.read(File.dirname(__FILE__) +  "/constants/departments.json"))
 
-p departments.first
+if Instructor.all.length == 0
+	instructors = create_instructors(10000)
+	puts 'Created 10000 instructors'
+else
+	instructors = Instructor.all.map { |instr| instr.id }
+end
+
+ActiveRecord::Base.transaction do
+	departments.each do |dept|
+		10.times do
+			p "Starting sql query..."
+			course_partition = []
+			100000.times do
+				units = 2
+				grading_opts = "Optional"
+				is_graduate_course = true
+				max_class_size = 20
+				name = Faker::Educator.course
+
+				description = Faker::Company.bs
+				course_no = Faker::Lorem.characters(16)
+				instructor = instructors.sample
+				course_partition.push("('#{dept}','#{units}', '#{grading_opts}', '#{is_graduate_course}', '#{max_class_size}', '#{name}', '#{description}', '#{course_no}', '#{instructor}')")
+			end
+			course_sql = <<-SQL
+				INSERT INTO courses
+				(dept, units, grading_opts, is_graduate_course, max_class_size, name, description, course_no, instructor_id)
+				VALUES #{course_partition.join(", ")}
+				ON CONFLICT DO NOTHING
+			SQL
+			ActiveRecord::Base.connection.execute(course_sql)
+		end
+	end
+end
